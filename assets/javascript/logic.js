9a03b7a75a297b1900f5b7107e6036ad0941f3c0
Jason Goeble <jason.goeble@gmail.com>
2018-06-12 14:34:12 -0400

    final working updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,67 +14,63 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 var completeValidation = false;
+var inputErrorMessage = "Please enter values with correct formatting for your new train entry:";
 
 //this validation function will only fire if the submit button is clicked, and uses the values directly from the input form
 function formValidation()
 {
     // Get the value of the input fields
     var inputData = [$("#trainName-input").val().trim(), $("#destination-input").val().trim(), $("#frequency-input").val().trim(), $("#firstTime-input").val().trim()];
-    console.log(inputData);
 
     var validated = [];
     completeValidation = false;
 
-    //preliminary check to make sure there is a value in each field, as they are all required
-    for (i=0; i<inputData.length; i++)
+    //preliminary check to make sure there is a value for train name
+    if (inputData[0] == "")
     {
-        if (inputData[i] == "")
-        {
-            alert("Please fill in all required fields");
-            validated.push(false);
-        }
-        else
-        {
-            validated.push(true);
-        }
-        //by the end of this for loop, array validated should have 4 values
-    }
-
-
-    //next, check to make sure that the frequency value is a number
-    //this will only run if the validated array reflects that there is a value for both trainName and destination
-    if (validated[0] && validated[1] === true)
-    {
-        // If frequency is not a number or <1, update validated array position 2
-        if (isNaN(inputData[2]) || inputData[2] < 1)
-        {
-            alert("Please input only numbers with a value of at least 1");
-            validated[2] = false;    
-        } 
-        else {
-            //frequency is a number
-            validated[2] = true;
-        }
-    }
-
-    //lastly, if train name, destination and frequency have all been validated move on to validate the first train time input
-    if (validated[0] && validated[1] & validated[2] === true)
-    {
-        //moment.js will validate that the first train time input is valid
-        if (moment(inputData[3],"HH:mm").isValid())
-        {
-            //update my validation tracking array that the time input is valid
-            validated[3] = true;
-        }
-        else
-        {
-            alert("Please input a first train time in military format - HH:mm");
-        }
+        validated.push(false);
+        inputErrorMessage = inputErrorMessage+" Name ";
     }
     else
     {
-
+        validated.push(true);
     }
+
+    //preliminary check to make sure there is a value for destination
+    if (inputData[1] == "")
+    {
+        inputErrorMessage = inputErrorMessage+" Destination ";
+        validated.push(false);
+    }
+    else
+    {
+        validated.push(true);
+    }
+
+    //next, check to make sure that the frequency value is a number
+    // If frequency is not a number or <1, update validated array position 2
+    if (isNaN(inputData[2]) || inputData[2] < 1 || inputData[2] == "")
+    {
+        inputErrorMessage = inputErrorMessage+" Frequency ";
+        validated.push(false);    
+    } 
+    else {
+        //frequency is a number
+        validated.push(true);
+    }
+
+    //moment.js will validate that the first train time input is valid
+    if (moment(inputData[3],"HH:mm").isValid() && inputData[3] != "")
+    {
+        //update my validation tracking array that the time input is valid
+        validated.push(true);
+    }
+    else
+    {
+        inputErrorMessage = inputErrorMessage+" First Arrival Time";
+        validated.push(false);
+    }
+   
 
     //if all user data has been validated
     if (validated[0] && validated [1] && validated[2] & validated[3] == true)
@@ -84,7 +80,6 @@ function formValidation()
     else
     {
         completeValidation = false;
-        alert("Please make sure your input data matches the stated requirements and also makes sense...Doug");
     }
 
     return(completeValidation);
@@ -100,14 +95,10 @@ $("#add-train").click(function(event)
     {
         //capture values provided by user into variables for push to database
         var trainName = $("#trainName-input").val().trim();
-        console.log("Train Name: "+trainName);
         var destination = $("#destination-input").val().trim();
-        console.log("Destination: "+destination);
         //these two variables require some manipulation to get both the right formatting and to permit derivation of additional information 
         var frequency = $("#frequency-input").val().trim();
-        console.log("Frequency: "+frequency);
         var firstArrival = $("#firstTime-input").val().trim();
-        console.log("First Arrival: "+firstArrival);
         //push validated user values (post a little manipulation) to database
         database.ref().push(
             {
@@ -118,48 +109,46 @@ $("#add-train").click(function(event)
                 dateAdded: firebase.database.ServerValue.TIMESTAMP
             });
     }
+    //if there are errors in the input, no firebase insert occurs and the error message indicating problems is alerted to the screen
     else
     {
-        alert("Your data is not in the right format....Doug!");
+        alert(inputErrorMessage);
     }
 
     //clear the form fields for the user
     $("#trainName-input").val("");
     $("#destination-input").val("");
     $("#frequency-input").val("");
-    $("firstArrival-input").val("");
+    $("#firstTime-input").val("");
+
+    //reset the validation indicator
+    completeValidation = false;
+
+    //reset the input error message
+    inputErrorMessage = "Please enter values with correct formatting for your new train entry:";
 });
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey)
+database.ref().on("child_added", function(childSnapshot)
 {
     //retrieve data from the database as a child object is added
     var showName = childSnapshot.val().trainName;
     var showDestination = childSnapshot.val().destination;
     var showFrequency = childSnapshot.val().frequency;
-    var showFirstArrival = childSnapshot.val().firstArrival;
 
     //convert the first arrival time
     var firstTimeConv = moment(childSnapshot.val().firstArrival, "HH:mm").subtract(1,"years");
-    console.log("First Time Conv: "+firstTimeConv);
     //pull the current time and put it into military time
     var currentTime = moment();
-    console.log("Current Time: "+currentTime);
     currentTime = moment(currentTime).format("hh:mm");
-    console.log("Current Time Formatted: "+currentTime);
     //calculate the difference between the times
     var diffTime = moment().diff(moment(firstTimeConv), "minutes");
-    console.log("Time Difference: "+diffTime);
     //the remainder of the time
     var timeRemainder = diffTime % childSnapshot.val().frequency;
-    console.log("Remainder: "+timeRemainder);
     //the minutes until the train arrives at the depot based upon the stated frequency
     var timeTillTrain = childSnapshot.val().frequency - timeRemainder;
-    console.log("Time Until Train: "+timeTillTrain);
     //formatted next arrival time of train
     var nextArrival = moment().add(timeTillTrain, "minutes");
-    console.log("Next Arrival: "+nextArrival);
     nextArrival = moment(nextArrival).format("hh:mm");
-    console.log("Next Arrival formatted: "+nextArrival);
 
     var shownextArrival = nextArrival;
     var showminutesAway = timeTillTrain;

44129062b4b00be4fd69eceb616a452d4c26cc20
Jason Goeble <jason.goeble@gmail.com>
2018-06-12 11:44:02 -0400

    updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,10 @@
-
 // Initialize Firebase
 var config = {
 apiKey: "AIzaSyAoPBO6KsujNPQYP71DmLamOYs-L8yTNLk",
 authDomain: "trainschedule-e8334.firebaseapp.com",
 databaseURL: "https://trainschedule-e8334.firebaseio.com",
 projectId: "trainschedule-e8334",
-storageBucket: "",
+storageBucket: "trainschedule-e8334.appspot.com",
 messagingSenderId: "76674777642"
 };
 
@@ -14,15 +13,17 @@ firebase.initializeApp(config);
 //create a variable to reference the database
 var database = firebase.database();
 
-function formValidation()
+//this validation function will only fire if the submit button is clicked, and uses the values directly from the input form
+/*function formValidation()
 {
-    // Get the value of the input field with id="numb"
-    var inputData = [$("#name-input").val().trim(), $("#role-input").val().trim(), $("#startDate-input").val().trim(), $("#monthlyRate").val().trim()];
+    // Get the value of the input fields
+    var inputData = [$("#trainName-input").val().trim(), $("#destination-input").val().trim(), $("#frequency-input").val().trim(), $("#firstTime-input").val().trim()];
+    console.log(inputData);
 
     var validated = [];
     var completeValidation = false;
 
-    //preliminary check to make sure there is a value in each field, as thehy are required
+    //preliminary check to make sure there is a value in each field, as they are all required
     for (i=0; i<inputData.length; i++)
     {
         if (inputData[i] == "")
@@ -34,32 +35,43 @@ function formValidation()
         {
             validated.push(true);
         }
+        //by the end of this for loop, array validated should have 4 values
     }
 
 
-    //next, check to make sure that the start date is correctly formatted and that the monthly rate is a number
+    //next, check to make sure that the frequency value is a number
+    //this will only run if the validated array reflects that there is a value for both trainName and destination
     if (validated[0] && validated[1] === true)
     {
-        //check to make sure start date is formatted MM/DD/YYYY
-        if (moment(validated[2], 'MM/DD/YYYY', true).isvalid)
+        // If frequency is not a number or <1, update validated array position 2
+        if (isNaN(inputData[2]) || inputData[2] < 1)
         {
+            alert("Please input only numbers with a value of at least 1");
+            validated[2] = false;    
+        } 
+        else {
+            //frequency is a number
             validated[2] = true;
+        }
+    }
+
+    //lastly, if train name, destination and frequency have all been validated move on to validate the first train time input
+    if (validated[0] && validated[1] & validated[2] === true)
+    {
+        //moment.js will validate that the first train time input is valid
+        if (moment(inputData[4],"HH:mm").isValid())
+        {
+            //update my validation tracking array that the time input is valid
+            validated[3] = true;
         }
         else
         {
-            validated[2] = false;
+            alert("Please input a first train time in military format - HH:mm");
         }
+    }
+    else
+    {
 
-        //check to make sure monthly rate is a number
-        // If monthly rate is Not a Number or less than one
-        if (isNaN(inputData[3]) || inputData[3] < 1)
-        {
-            validatd[3] = false;    
-        } 
-        else {
-            //monthly rate is a number
-            validated[3] = true;
-        }
     }
 
     //if all user data has been validated
@@ -67,54 +79,115 @@ function formValidation()
     {
         completeValidation = true;
     }
-    //if the start date or monthly rate are not in valid formats or data types
     else
     {
         completeValidation = false;
-        alert("Please make sure your Start Date is in the format of MM/DD/YYYY and your monthly rate is a number.")
+        alert("Please make sure your input data matches the stated requirements and also makes sense...Doug");
     }
 
     return(completeValidation);
-}
+}*/
 
-$("#submit").click(function(event)
+$("#add-train").click(function(event)
 {
     event.preventDefault();
 
-    //capture values provided by user
-    var name = $("#name-input").val().trim();
-    var role = $("#role-input").val().trim();
-    var startDate = $("#startDate-input").val().trim();
-    var monthlyRate = $("#monthlyRate").val().trim();
+    //call the user input validation function
+    //hopefully prevents the inclusion of garbage data from the database....gigo
+   // if (formValidation())
+    //{
 
-    //push validated user values to database
+    //capture values provided by user into variables for push to database
+    var trainName = $("#trainName-input").val().trim();
+    console.log("Train Name: "+trainName);
+    var destination = $("#destination-input").val().trim();
+    console.log("Destination: "+destination);
+    //these two variables require some manipulation to get both the right formatting and to permit derivation of additional information 
+    var frequency = $("#frequency-input").val().trim();
+    console.log("Frequency: "+frequenncy);
+    var firstArrival = $("firstArrival-input").val().trim();
+    console.log("First Arrival: "+firstArrival);
+    //convert the first arrival time
+    var firstTimeConv = moment(firstArrival, "HH:mm").subtract(1,"years");
+    console.log("First Time Conv: "+firstTimeConv);
+    //pull the current time and put it into military time
+    var currentTime = moment();
+    console.log("Current Time: "+currentTime);
+    currentTime = moment(currentTime).format("hh:mm");
+    console.log("Current Time Formatted: "+currentTime);
+    //calculate the difference between the times
+    var diffTime = moment().diff(moment(firstTimeConv), "minutes");
+    console.log("Time Difference: "+diffTime);
+    //the remainder of the time
+    var timeRemainder = diffTime % frequency;
+    console.log("Remainder: "+timeRemainder);
+    //the minutes until the train arrives at the depot based upon the stated frequency
+    var timeTillTrain = frequency - timeRemainder;
+    console.log("Time Until Train: "+timeTillTrain);
+    //formatted next arrival time of train
+    var nextArrival = moment().add(timeTillTrain, "minutes");
+    console.log("Next Arrival: "+nextArrival);
+    nextArrival = moment(nextArrival).format("hh:mm");
+    console.log("Next Arrival formatted: "+nextArrival);
+    //push validated user values (post a little manipulation) to database
     database.ref().push(
         {
-            name: name,
-            role: role,
-            startDate: startDate,
-            monthlyRate: monthlyRate,
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            firstArrival: firstArrival,
+            nextArrival: nextArrival,
+            minutesAway: timeTillTrain,
             dateAdded: firebase.database.ServerValue, TIMESTAMP
         });
+    //}
+   // else
+    //{
 
+    //}
+
+    //clear the form fields for the user
+    $("#trainName-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("firstArrival-input").val("");
 });
 
-database.ref().on("child_added", function(childSnapshot)
+database.ref().on("value", function(snapshot)
 {
-    //some kind of operation here        
+},
+function(errorObject) 
+{
+    // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+});
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey)
+{
+    //retrieve data from the database as a child object is added
+    var showName = childSnapshot.val().trainName;
+    var showDestination = childSnapshot.val().destination;
+    var showFrequency = childSnapshot.val().frequency;
+    var shownextArrival = childSnapshot.val().nextArrival;
+    var showminutesAway = childSnapshot.val().minutesAway;
+
+    //append the newly added child object attributes to the table for display to the user
+    //because the table allows the data to perpetuate infinitiely, all data (regardless of user) will be visible to all visitors
+    $("#displayTable").append("<tr><td>"+showName+"</td><td>"+showDestination+"</td><td>"+showFrequency+"</td><td>"+shownextArrival+"</td><td>"+showminutesAway+"</td></tr>");
 },
 function (errorObject) 
 {
         console.log("Errors handled: " + errorObject.code);
 });
 
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot)
-{
-    console.log(snapshot.key);
-    var showName = snapshot.val().name;
-    var showRole = snapshot.val().role;
-    var showDate = snapshot.val().startDate;
-    var showRate = snapshot.val().monthlyRate;
+//database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot)
+//{
+//    console.log(snapshot.key);
+//    var showName = childSnapshot.val().trainName;
+//    var showDestination = childSnapshot.val().destination;
+//    var showFrequency = childSnapshot.val().frequency;
+//    var shownextArrival = childSnapshot.val().nextArrival;
+//    var showminutesAway = childSnapshot.val().minutesAway;
 
-    $("#displayTable").append("<tr><td>"+showName+"</td><td>"+showRole+"</td><td>"+showDate+"</td><td>"+showRate+"</td></tr>");
-});
+    //$("#displayTable").append("<tr><td>"+showName+"</td><td>"+showDestination+"</td><td>"+showFrequency+"</td><td>"+shownextArrival+"</td><td>"+showminutesAway+"</td></tr>");
+//});
